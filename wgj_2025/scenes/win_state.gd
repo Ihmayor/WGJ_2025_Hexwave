f208ac1d99809b7503444ba02a9a6addd95d2524
IM <irenehmayor>
2025-08-12 22:19:16 -0700

    final tweaks from me, ping me for animation things

--- wgj_2025/scenes/win_state.gd
+++ wgj_2025/scenes/win_state.gd
@@ -6,7 +6,7 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 	if (body is not Player):
 		return
 	theme.stop()
-	#$AreaAmbience.play()
-	#$AreaMusic.play()
+	$AreaAmbience.play()
+	$AreaMusic.play()
 	await get_tree().create_timer(2).timeout
 	win_screen.on_win()
